c7656db8c3903c7614b7de4046dab621982328fe
SantuyoChristleJoyce <sm201600459@wmsu.edu.ph>
2020-11-20 00:19:28 +0800

    Brew_Crew

---
+++ lib/models/user.dart
@@ -0,0 +1,14 @@
+class Person {
+  final String uid;
+
+  Person({this.uid});
+}
+
+class UserData {
+  final String uid;
+  final String name;
+  final String sugars;
+  final int strength;
+
+  UserData({this.uid, this.sugars, this.strength, this.name});
+}
